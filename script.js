e7c5468d2353bf8428348b8535683e0bbd424eee
Zac Eskridge <zace118@gmail.com>
2019-12-28 14:11:04 -0700

    Wrote out the AJAX call to paste the pulled responses to the jumbotron.

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 const cityName = "Austin";
+let m =moment().format('dddd, MMMM Do YYYY');
+
 // const cityName = $('#citySearch').val();
 
-console.log(cityName);
+$('#currentDate').text(m);
+
+
+
 
 $.ajax({
-    url:"api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=dd0390e9886af8c80bbda292ef25a74c",
+    url:"https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=dd0390e9886af8c80bbda292ef25a74c",
     method: "GET"
 }).then(function(res){
     console.log(res);
+    $('#cityName').text(res.name);
+
+    let kelvinTemp = res.main.temp;
+    let fahrenheitTemp = (Math.floor(((kelvinTemp - 273.15) * 1.80 + 32))); 
+    $('#cityTemp').text(fahrenheitTemp + '°')
+
+    $('#cityHumidity').text(res.main.humidity + '%');
+
+    $('#cityWind').text(res.wind.speed + ' mph');
+
+    const lat = res.coord.lat;
+    const lon = res.coord.lon;
+
+    $.ajax({
+        url:'https://api.openweathermap.org/data/2.5/uvi?appid=dd0390e9886af8c80bbda292ef25a74c&lat=' + lat + '&lon=' + lon,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        $("#cityUVI").text(response.value)
+    })
 })
