8b402c1b20506455e0bec4f68ede3ae14a8fd44d
Zac Eskridge <zace118@gmail.com>
2019-12-29 12:26:56 -0700

    Got the formating correct for the most part. Need to figure out where I want the cities to go once they've been searched. Added some notes in the script file, AND got the icons to work (without using an if statement)

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const cityName = "Denver";
+// const cityName = $('#citySearch').val();
+
+// Variables and jQuery to set dates
 let m = moment().format('dddd, MMMM Do YYYY');
 let forecast1 =moment().add(1,'d').format('l');
 let forecast2 =moment().add(2,'d').format('l');
 let forecast3 =moment().add(3,'d').format('l');
 let forecast4 =moment().add(4,'d').format('l');
 let forecast5 =moment().add(5,'d').format('l');
-
-// const cityName = $('#citySearch').val();
 
 $('#currentDate').text(m);
 $('#nextDay1').text(forecast1);
@@ -15,28 +16,35 @@ $('#nextDay3').text(forecast3);
 $('#nextDay4').text(forecast4);
 $('#nextDay5').text(forecast5);
 
+
+// AJAX call to open weather api. Contains another AJAX call for the UVI
 $.ajax({
     url:"https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=dd0390e9886af8c80bbda292ef25a74c",
     method: "GET"
 }).then(function(res){
-    // console.log(res);
+    console.log(res);
     $('#cityName').text(res.name);
+    
 
-    const currentW = res.weather[0].main;
-    console.log(currentW);
+    // Pulling and setting the icons
+    const currentIcon = res.weather[0].icon;
+    // console.log(currentIcon);
+    const iconURL = "http://openweathermap.org/img/wn/"+currentIcon+"@2x.png"
+    $('#cityName').append("<img src="+iconURL+">");
 
-    if (currentW === "Clear") {
-        $('#icon').attr("src", "Assets/Images/Sunny.png")
-    }
 
+    // Temperature variables and such
     let kelvinTemp = res.main.temp;
     let fahrenheitTemp = (Math.floor(((kelvinTemp - 273.15) * 1.80 + 32))); 
     $('#cityTemp').text(fahrenheitTemp + '°')
 
+    // Pulling and setting humidity
     $('#cityHumidity').text(res.main.humidity + '%');
 
+    // Pulling and setting windspeed
     $('#cityWind').text(res.wind.speed + ' mph');
 
+    //Setting variables for latitude and longitude to be used in the next AJAX call to pull and set UVI
     const lat = res.coord.lat;
     const lon = res.coord.lon;
 
@@ -44,7 +52,6 @@ $.ajax({
         url:'https://api.openweathermap.org/data/2.5/uvi?appid=dd0390e9886af8c80bbda292ef25a74c&lat=' + lat + '&lon=' + lon,
         method: "GET"
     }).then(function(response){
-        // console.log(response);
         $("#cityUVI").text(response.value)
     })
 })
