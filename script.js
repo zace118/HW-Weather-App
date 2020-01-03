88ff24c49bd882424105c87550eee59427e5f8f5
Zac Eskridge <zace118@gmail.com>
2020-01-03 16:14:03 -0700

    Got the geolocation to work 100% and was able to delete the extra cards the loop was making every loop.

--- script.js
+++ script.js
@@ -1,41 +1,22 @@
-// Variables and jQuery to set dates
-let m = moment().format('dddd, MMMM Do YYYY');
-let forecast1 = moment().add(1,'d').format('l');
-let forecast2 = moment().add(2,'d').format('l');
-let forecast3 = moment().add(3,'d').format('l');
-let forecast4 = moment().add(4,'d').format('l');
-let forecast5 = moment().add(5,'d').format('l');
-
-$('#currentDate').text(m);
-$('#nextDay1').text(forecast1);
-$('#nextDay2').text(forecast2);
-$('#nextDay3').text(forecast3);
-$('#nextDay4').text(forecast4);
-$('#nextDay5').text(forecast5);
-
-
 // ------Geolocation stuff------
     
 function getLocation(){
-    
     if (navigator.geolocation){
         const currentPosition = navigator.geolocation.getCurrentPosition(showPosition);
-        console.log(currentPosition);
     }
-    else{
+    else {
         alert("Geolocation is not supported by this browser.");
     }
-
 }
 
 function showPosition(position){
 
-    console.log(position);
+    // console.log(position);
 
     const geoLat = position.coords.latitude; 
     const geoLon = position.coords.longitude;
-    console.log(geoLat);
-    console.log(geoLon);
+    // console.log(geoLat);
+    // console.log(geoLon);
 
     $.ajax({
         url:'https://api.openweathermap.org/data/2.5/uvi?appid=dd0390e9886af8c80bbda292ef25a74c&lat=' + geoLat + '&lon=' + geoLon,
@@ -48,7 +29,7 @@ function showPosition(position){
         url:'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + geoLat + ',' + geoLon + '&key=AIzaSyBpZZdD3TjpeQHhp9EV63t1sE4PIPTqo58',
         method: 'GET'
     }).then(function(res){
-        // console.log(res);
+        console.log(res);
         console.log(res.results[6].formatted_address);
         const geoCityName = res.results[6].formatted_address;
         getAJAX(geoCityName);
@@ -91,7 +72,7 @@ function searchedCities(getButton) {
 $("#searchButton").on('click', function(){
     
     const cityName = $('#searchBar').val();
-    console.log(cityName);
+    // console.log(cityName);
     getAJAX(cityName);
     $("#searchBar").val('');
 
@@ -106,6 +87,10 @@ $("#searchButton").on('click', function(){
 // ------Main function that puts all the info on the page------
 
 function getAJAX(someCity){
+
+//--------Emptying the cards between searches!!!!!!-----------
+    const a = $("#cardSlot").children();
+    a.remove();
     $('#cityName').empty();
     $('#cityTemp').empty();
     $('#cityHumidity').empty();
@@ -116,7 +101,7 @@ function getAJAX(someCity){
         url:"https://api.openweathermap.org/data/2.5/weather?q=" + someCity + "&units=imperial&appid=dd0390e9886af8c80bbda292ef25a74c",
         method: "GET"
     }).then(function(res){
-        console.log(res);
+        // console.log(res);
         $('#cityName').text(res.name);
     
 
@@ -142,7 +127,7 @@ function getAJAX(someCity){
         // This is the AJAX call to pull and set the UVI
         $.ajax({
             url:'https://api.openweathermap.org/data/2.5/uvi?appid=dd0390e9886af8c80bbda292ef25a74c&lat=' + lat + '&lon=' + lon,
-            // method: "GET"
+            method: "GET"
         }).then(function(response){
             $("#cityUVI").text(response.value)
         })
@@ -154,86 +139,95 @@ function getAJAX(someCity){
             url:"https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&units=imperial&appid=dd0390e9886af8c80bbda292ef25a74c",
             method: "GET"
         }).then(function(res){
-            console.log(res);
+                console.log(res);
+                console.log((res.list[0].dt_txt).slice(5,10));
 
-            $('#icon1').empty();
-            $('#icon2').empty();
-            $('#icon3').empty();
-            $('#icon4').empty();
-            $('#icon5').empty();
-            $('#cardTemp1').empty();
-            $('#cardTemp2').empty();
-            $('#cardTemp3').empty();
-            $('#cardTemp4').empty();
-            $('#cardTemp5').empty();
-            $('#cardHumidity1').empty();
-            $('#cardHumidity2').empty();
-            $('#cardHumidity3').empty();
-            $('#cardHumidity4').empty();
-            $('#cardHumidity5').empty();
+            //This is the for loop that 
+            for (let i = 0; i < res.list.length; i++) {
+                if (res.list[i].dt_txt.indexOf("12:00") !== -1){  
 
-            // for (let i = 0; i < res.list.length; i++) {
-            //     if (res.list[i].dt_txt.indexOf("12:00") !== -1){
-            //         const cardIcon1 = res.list[i].weather[0].icon;
-            //         const icon1URL = "http://openweathermap.org/img/wn/"+cardIcon1+"@2x.png"
-            
-            //         $('#icon0').append("<img src="+icon1URL+">");
-            //         $('#cardTemp0').append(res.list[i].main.temp + '°');
-            //         $('#cardHumidity0').append(res.list[i].main.humidity + '%');
-            //     }
-                
-            // }
+                    // $(".card").remove();
 
-            // Everytime it loops, create a new car with class "card-body", etc..., then append the card to the row
+                    $('#icon' + i).empty();
+                    // // $('#icon1').empty();
+
+                    $('#cardTemp' + i).empty();
+                    // // $('#cardTemp1').empty();
+
+                    $('#cardHum' + i).empty();
+                    // $('#cardHumidity1').empty();
 
 
+                    const cardIcon = res.list[i].weather[0].icon;
+                    // console.log(cardIcon);
+                    const iconURL = "http://openweathermap.org/img/wn/"+cardIcon+"@2x.png"
+                    // console.log(iconURL);
 
 
-            // Card 1
-            const cardIcon1 = res.list[0].weather[0].icon;
-            const icon1URL = "http://openweathermap.org/img/wn/"+cardIcon1+"@2x.png"
-            
-            $('#icon1').append("<img src="+icon1URL+">");
-            $('#cardTemp1').append(res.list[0].main.temp + '°');
-            $('#cardHumidity1').append(res.list[0].main.humidity + '%');
+                    //Generates the div tg to begin creating the cards.
+                    const parentDiv = $("<div>");
+                    //Adds class and ID to parentDiv
+                    parentDiv.addClass("card").attr('id', 'day' + i).attr('style', 'width:13rem;');
 
 
-            // Card 2
-            const cardIcon2 = res.list[8].weather[0].icon;
-            const icon2URL = "http://openweathermap.org/img/wn/"+cardIcon2 +"@2x.png"
-    
-            $('#icon2').append("<img src="+icon2URL+">");
-            $('#cardTemp2').append(res.list[8].main.temp + '°');
-            $('#cardHumidity2').append(res.list[8].main.humidity + '%');
+                    //Generates the div tag for the cards
+                    const childDiv= $("<div>");
+                    //Adds class to childDiv1
+                    childDiv.addClass("card-body");
 
 
-            // Card 3
-            const cardIcon3 = res.list[16].weather[0].icon;
-            const icon3URL = "http://openweathermap.org/img/wn/"+cardIcon3+"@2x.png"
-    
-            $('#icon3').append("<img src="+icon3URL+">");
-            $('#cardTemp3').append(res.list[16].main.temp + '°');
-            $('#cardHumidity3').append(res.list[16].main.humidity + '%');
+                    // Generates the h5 tag for the cards
+                    const hFive = $("<h5>");
+                    //Adds class and ID to h5
+                    hFive.addClass("card-title").attr("id", "nextDay" + i);
 
 
-             // Card 4
-             const cardIcon4 = res.list[24].weather[0].icon;
-             const icon4URL = "http://openweathermap.org/img/wn/"+cardIcon4+"@2x.png"
-     
-             $('#icon4').append("<img src="+icon4URL+">");
-             $('#cardTemp4').append(res.list[24].main.temp + '°');
-             $('#cardHumidity4').append(res.list[24].main.humidity + '%');
+                    // Generates the h6 tag for the cards
+                    const hSix = $("<h6>");
+                    //Adds ID to h6
+                    hSix.attr("id", "icon" + i);
 
 
-             // Card 5
-             const cardIcon5 = res.list[36].weather[0].icon;
-             const icon5URL = "http://openweathermap.org/img/wn/"+cardIcon5+"@2x.png"
-     
-             $('#icon5').append("<img src="+icon5URL+">");
-             $('#cardTemp5').append(res.list[36].main.temp + '°');
-             $('#cardHumidity5').append(res.list[36].main.humidity + '%');
+                    //Generates the b tag for Temp for the cards
+                    const tempTag = $("<b>Temp: </b>");
+                    //Adds id to the b tag
+                    tempTag.attr("id", "cardTemp" + i);
 
 
+                    //Generates the b tag for the Humidity for the cards
+                    const humTag =$("<b>Humidity: </b>");
+                    //Adds id to the b tag
+                    humTag.attr("id", "cardHum" +i);
+
+                    // Creating the cards, dynamically
+                    $(parentDiv).append(childDiv);
+                    $(childDiv).append(hFive);
+                    $(childDiv).append(hSix);
+                    $(childDiv).append(tempTag);
+                    $(childDiv).append($("<br>"));
+                    $(childDiv).append(humTag);
+                    // Appending the cards to the cardSlot ID in the aside in the HTML
+                    $("#cardSlot").append(parentDiv);   
+
+                    // Using moment.js to set today's date
+                    let m = moment().format('dddd, MMMM Do YYYY');
+                    //Using the procided date, sliced out of the data_text within the JSON
+                    let forecast = ((res.list[i].dt_txt).slice(5,10));
+                        // console.log(forecast);
+
+
+                    // Pasting all the info onto the cards, dynamically
+                    $('#currentDate').text(m);
+                    $('#nextDay' + i).text(forecast);
+                    $('#icon' + i).append("<img src="+iconURL+">");
+                    $('#cardTemp' + i).append(res.list[i].main.temp + '°');        $('#cardHum' + i).append(res.list[i].main.humidity + '%');
+
+                    
+                    
+
+
+                } 
+            }
         })
     })
 }
